989fa40817aa5436656168880032eb0b8b7a9b7c
torvalds <torvaldz@gmail.com>
2015-08-23 15:27:05 +0200

    First commit

---
+++ SlimscrollAsset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace torvalds\slimscroll;
+
+use \yii\web\AssetBundle;
+
+class SlimscrollAsset extends AssetBundle {
+
+public $sourcePath = '@bower/slimscroll';
+public $js = [
+'jquery.slimscroll.min.js'
+];
+}
